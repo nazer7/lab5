b62f3e7deeddcc24937fc147df1c94cde4655ff2
nazer7 <nazerkebekbosyn932@gmail.com>
2025-02-22 11:56:42 +0300

    Add files via upload

---
+++ Python_RegEx_exercises1.py
@@ -0,0 +1,9 @@
+import re
+text=input("enter word-")
+def text_match(text):
+        patterns = '^a(b*)$'
+        if re.search(patterns,  text):
+                return 'Found a match!'
+        else:
+                return('Not matched!')
+print(text_match(text))
